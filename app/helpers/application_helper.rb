9c39005fd8fb241fd2b787b242652b7d9061bcbf
abp437 <abp437@gmail.com>
2021-11-02 18:39:29 +0530

    Add svg file loader and live reload for scss files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
     # Here %B stands for readable month
     DateTime.now.to_date.strftime('%d %B, %Y')
   end
+
+  def load_svg(path)
+    File.open("app/assets/images/svg/#{path}", "rb") do |file|
+      raw file.read
+    end
+  end
 end
