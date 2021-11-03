9e01819641dddd30b6c755f5ee2a44aa697e5a18
abp437 <abp437@gmail.com>
2021-11-03 14:06:59 +0530

    Add validations & improvise ext to be shown

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,7 @@
 class Entry < ApplicationRecord
+  validates :calories, :proteins, :carbohydrates, :fats, :meal_type, presence: true
+  validates :calories, :proteins, :carbohydrates, :fats, numericality: true
+
   def day
     self.created_at.strftime(Entry.date_format)
   end
