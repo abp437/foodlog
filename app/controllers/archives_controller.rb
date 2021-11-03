f22dd8b7833ed4da464cc576d5f13ff2d1ebfef8
abp437 <abp437@gmail.com>
2021-11-03 11:24:33 +0530

    Add notes for pluck & group_by methods

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,5 +1,7 @@
 class ArchivesController < ApplicationController
   def index
+    # `&:method_name` is basically a call to an instance method on self
+    # `group_by` groups similar entries into one array
     @entries = Entry.all.group_by(&:day)
   end
 end
