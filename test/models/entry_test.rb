60d16c624fa32598cc46953543258ea11d8f0d2f
abp437 <abp437@gmail.com>
2021-11-03 14:42:08 +0530

    Add first test for TDD

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test "is valid with valid attributes" do
+    entry = Entry.new(meal_type: 'Breakfast', calories: 23, fats: 34, carbohydrates: 233, proteins: 56)
+    assert entry.save
+  end
 end
